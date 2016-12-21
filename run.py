8e411bd649ffbf18ca5eb4a107094841dcc74a73
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-12-20 18:01:51 -0800

    Properly initialize logging and dont log errors on expected 404s

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import base64
 import logging
 
@@ -6,19 +7,22 @@ import rsyslog
 from curio_http import ClientSession
 from asynctcp import AsyncTCPCallbackServer
     
-rsyslog.setup()
+rsyslog.setup(log_level = os.environ['LOG_LEVEL'])
 LOGGER = logging.getLogger(__name__)
 
 async def get_downloads(package_name):
     async with ClientSession() as session:
         url = 'https://api.npmjs.org/downloads/point/last-week/{}'.format(package_name)
         response = await session.get(url)
-        if (response.status_code < 200) or (response.status_code >= 300):
+        if response.status_code == 404:
+            LOGGER.debug('No such package {}'.format(package_name))
+            return 0
+        elif (response.status_code < 200) or (response.status_code >= 300):
             LOGGER.error('GET of {} returned {}'.format(url, response.status_code))
             return 0
         content = await response.json()
         downloads = content['downloads'] if 'downloads' in content else 0
-        LOGGER.info('number of downloads for package {} is {}'.format(package_name, downloads))
+        LOGGER.debug('number of downloads for package {} is {}'.format(package_name, downloads))
         return downloads
 
 if __name__ == '__main__':
