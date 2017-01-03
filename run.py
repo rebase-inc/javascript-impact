59d9660d417c00884a1351a2f059248b2c25dcd8
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-02 21:51:18 -0800

    Switch to using json blob instead of bytestring

--- run.py
+++ run.py
@@ -1,35 +1,34 @@
 import os
+import json
 import base64
 import logging
+
+from multiprocessing import current_process
 
 import rsyslog
 
 from curio_http import ClientSession
-from asynctcp import AsyncTCPCallbackServer
-    
-rsyslog.setup(log_level = os.environ['LOG_LEVEL'])
-LOGGER = logging.getLogger(__name__)
+from asynctcp import AsyncTcpCallbackServer
 
-async def get_downloads(package_name):
+current_process().name = os.environ['HOSTNAME']
+rsyslog.setup(log_level = os.environ['LOG_LEVEL'])
+LOGGER = logging.getLogger()
+
+async def get_downloads(json_request):
     async with ClientSession() as session:
-        url = 'https://api.npmjs.org/downloads/point/last-week/{}'.format(package_name)
+        url = 'https://api.npmjs.org/downloads/point/last-week/{}'.format(json_request['module'])
         response = await session.get(url)
         if response.status_code == 404:
-            LOGGER.debug('No such package {}'.format(package_name))
-            return 0
+            LOGGER.debug('No such package {}'.format(json_request['module']))
+            data = { 'impact': 0 }
         elif (response.status_code < 200) or (response.status_code >= 300):
             LOGGER.error('GET of {} returned {}'.format(url, response.status_code))
-            return 0
-        content = await response.json()
-        downloads = content['downloads'] if 'downloads' in content else 0
-        LOGGER.debug('number of downloads for package {} is {}'.format(package_name, downloads))
-        return downloads
+            data = { 'error': errno.EIO, 'impact': None }
+        else:
+            content = await response.json()
+            data = { 'impact': content['downloads'] if 'downloads' in content else 0 }
+            LOGGER.debug('Impact for module {} is {}'.format(json_request['module'], data['impact']))
+        return json.dumps(data)
 
 if __name__ == '__main__':
-    AsyncTCPCallbackServer(
-            callback = get_downloads, 
-            host = '0.0.0.0',
-            port = 9999,
-            encode = lambda d: bytes(str(d) + '\n', 'utf-8'),
-            decode = lambda d: base64.b64decode(d).decode().strip()
-            ).run()
+    AsyncTcpCallbackServer('0.0.0.0', 9999, get_downloads, memoized = True).run()
