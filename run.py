70f98ea8f0d23fe76bc311e43773649d958fe4e4
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-03 18:03:13 -0800

    Quiet curio_http logger

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ from asynctcp import AsyncTcpCallbackServer
 current_process().name = os.environ['HOSTNAME']
 rsyslog.setup(log_level = os.environ['LOG_LEVEL'])
 LOGGER = logging.getLogger()
+logging.getLogger('curio_http').setLevel(logging.WARNING)
 
 async def get_downloads(json_request):
     async with ClientSession() as session:
